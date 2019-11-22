bad1adaeaa059200d805822d78cbcf80bf8695ce
Mugiwara No Zaka <zakaelghoul@gmail.com>
2019-11-22 14:02:36 +0100

    Huis

--- 01_huisje/script.js
+++ 01_huisje/script.js
@@ -1,13 +1,47 @@
-// maak een variabele voor de canvas
 const canvas = document.getElementById('canvas');
 
-// leg een 2d-context over de canvas
 const context = canvas.getContext('2d');
 
-//maak de canvas schermvullend
 const width = window.innerWidth;
 const height = window.innerHeight;
 canvas.width = width;
 canvas.height = height;
 
-// hier kun je op de canvas tekenen
+context.beginPath()
+context.fillStyle = "red";
+context.moveTo(300, 100);
+context.lineTo(700, 200);
+context.lineTo(600, 400);
+context.lineTo(200, 300)
+context.closePath();
+context.stroke();
+context.fill()
+
+context.beginPath()
+context.fillStyle = "white";
+context.moveTo(200, 300)
+context.lineTo(200, 500)
+context.lineTo(600, 600)
+context.lineTo(600, 400)
+context.closePath();
+context.stroke();
+context.fill()
+
+context.beginPath()
+context.fillStyle = "white";
+context.moveTo(600, 400)
+context.lineTo(700, 200)
+context.lineTo(800, 300)
+context.closePath();
+context.stroke();
+context.fill()
+
+context.beginPath()
+context.fillStyle = "white";
+context.moveTo(600, 600)
+context.lineTo(800, 500)
+context.lineTo(800, 300)
+context.lineTo(600, 400)
+context.closePath();
+context.stroke();
+context.fill()
